5df92b5d878ad95deaa29125a973c321d7557f3b
Pas <pasquale.paudice.net@gmail.com>
2026-04-02 14:07:45 +0200

    Corse table in Dashboard

--- ames_frontend/src/views/Dashboard.vue
+++ ames_frontend/src/views/Dashboard.vue
@@ -4,33 +4,15 @@
 <template>
     <admin-layout>
 
-        <h1>Gestione Corse</h1>
-        
-        <div v-if="errore" style="color: red">{{ errore }}sss</div>
-
-        <ul v-else>
-        <li v-for="corsa in corse" :key="corsa.id">
-            <strong>
-                Descrizione: {{ corsa.description }}
-            
-            </strong> 
-            <p>
-            Tipo: {{ corsa.type }}
-            </p>
-            <!-- Se hai incluso i samples nel serializer -->
-            <ul>
-            <li v-for="s in corsa.samples" :key="s.sampleId">
-                SampleName: {{ s.sampleName }}
-            </li>
-            </ul>
-        </li>
-        </ul>
+    <h1 class="text-center mb-3">Gestione Corse</h1>
+    <BasicTableOne :data="corse" />
     </admin-layout>
 
 </template>
 
 <script setup>
 import AdminLayout from '../components/layout/AdminLayout.vue'
+import BasicTableOne from '../components/tables/basic-tables/BasicTableOne.vue'
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
@@ -41,7 +23,6 @@ const errore = ref(null)
 const fetchCorse = async () => {
   try {
     const response = await axios.get('http://127.0.0.1:8000/api/corsas/')
-    console.log('oooooo', response)
     corse.value = response.data.results
   } catch (err) {
     errore.value = "Errore nel caricamento dati: " + err.message
