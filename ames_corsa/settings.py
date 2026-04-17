aba686c0eb35a8bd18379e7a5cef5abe9d2323a4
Pas <pasquale.paudice.net@gmail.com>
2026-04-17 12:35:16 +0200

    set CACHES(redis) in settings.py

--- ames_corsa/settings.py
+++ ames_corsa/settings.py
@@ -130,6 +130,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1", 
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
