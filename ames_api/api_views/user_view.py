ec678229caa1f764becf0ff2bd59c92d75d2d680
Pas <pasquale.paudice.net@gmail.com>
2026-04-20 08:42:25 +0200

    feat: enhance UserSerializer to include password field and validation

--- ames_api/api_views/user_view.py
+++ ames_api/api_views/user_view.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets, permissions
 from ames_api.models import CustomUser
 from ames_api.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import generics
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+
 
     def get_permissions(self):
         if self.action in ['update', 'partial_update', 'destroy']:
             # Solo l'utente stesso o un admin può modificare/cancellare
             return [permissions.IsAuthenticated()] 
         return [permissions.AllowAny()]
+
--- ames_api/serializers.py
+++ ames_api/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import CustomUser, Corsa, Sample
+from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email','password']
 
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
 
 
 class SampleCorsaSerializer(serializers.ModelSerializer):
